5887816ed7f41ccbdb25cc11d64df339033c219c
Lucia <lucy_17_4@hotmail.com>
2021-11-01 18:45:31 -0300

    newcolecction celu

--- js/newcollection.js
+++ js/newcollection.js
@@ -1,11 +1,11 @@
 Interfaz.numeroCarrito();
 
 $(".banner").html(`
-<a href="../views/dresses.html"><img class="banner__img" src="../assets/img/banner1.jpg"/></a>
-<a href="../views/dresses.html"><img class="banner__img" src="../assets/img/banner2.jpg"/></a>
-<a href="../views/dresses.html"><img class="banner__img" src="../assets/img/banner3.jpg"/></a>
-<a href="../views/dresses.html"><img class="banner__img" src="../assets/img/banner4.jpg"/></a>
-<a href="../views/dresses.html"><img class="banner__img" src="../assets/img/banner5.jpg"/></a>
+<a href="../views/dresses.html"><img class="banner__img banner__img1" src="../assets/img/banner1.jpg"/></a>
+<a href="../views/dresses.html"><img class="banner__img banner__img2" src="../assets/img/banner2.jpg"/></a>
+<a href="../views/dresses.html"><img class="banner__img banner__img3" src="../assets/img/banner3.jpg"/></a>
+<a href="../views/dresses.html"><img class="banner__img banner__img4" src="../assets/img/banner4.jpg"/></a>
+<a href="../views/dresses.html"><img class="banner__img banner__img5" src="../assets/img/banner5.jpg"/></a>
 <p class="poem">"Infinite in mystery is the gift of the Goddess<br>
 We seek it thus, and take to the sky<br>
 Ripples form on the water's surface<br>
